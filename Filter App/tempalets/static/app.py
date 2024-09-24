b275d3852974458a4e9f7011602054ede950f0ef
Dhanyatha S <95542660+Dhanyatha-s@users.noreply.github.com>
2024-09-25 01:10:58 +0530

    Update app.py
    Signed-off-by: Dhanyatha S <95542660+Dhanyatha-s@users.noreply.github.com>

--- Filter App/tempalets/static/app.py
+++ Filter App/tempalets/static/app.py
@@ -1,1 +1,27 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__, template_folder='templates')
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    original_text = ''
+    filtered_text = ''
+
+    if request.method == 'POST':
+        original_text = request.form.get('text')
+        selected_filter = request.form.get('filter')
+
+        if selected_filter == 'uppercase':
+            filtered_text = original_text.upper()
+        elif selected_filter == 'capitalize':
+            filtered_text = original_text.capitalize()
+        elif selected_filter == 'reverse':
+            filtered_text = original_text[::-1]
+        else:
+            filtered_text = original_text
+
+    return render_template('index.html', original_text=original_text, filtered_text=filtered_text)
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
